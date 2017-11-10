6d3a17f828eabc2f7d73c9e91c248c917c709597
Anh Tuan Truong <atruon16@student.aau.dk>
2017-11-10 15:13:33 +0100

    Foderbestilling - UI

--- AalborgZooProjekt/AalborgZooProjekt/View/MainWindow.xaml.cs
+++ AalborgZooProjekt/AalborgZooProjekt/View/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
 using AalborgZooProjekt.ViewModel;
 using MahApps.Metro.Controls;
+using System.Windows.Threading;
+using System;
 
 namespace AalborgZooProjekt
 {
@@ -9,6 +11,24 @@ namespace AalborgZooProjekt
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        DispatcherTimer Timer = new DispatcherTimer();
+
+        public DateTime Time
+        {
+            get { return (DateTime)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        private string department = "Afrika";
+
+        public string Department
+        {
+            get { return department; }
+        }
+
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("Time", typeof(DateTime), typeof(MainWindow), new PropertyMetadata(DateTime.Now));
+
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
         /// </summary>
@@ -16,6 +36,22 @@ namespace AalborgZooProjekt
         {
             InitializeComponent();
             Closing += (s, e) => ViewModelLocator.Cleanup();
+
+            this.DataContext = this;
+            Timer.Tick += new EventHandler(TimerClick);
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Start();
+
+        }
+
+        private void TimerClick(object sender, EventArgs e)
+        {
+            Time = DateTime.Now;
+        }
+
+        private void TabControl_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
