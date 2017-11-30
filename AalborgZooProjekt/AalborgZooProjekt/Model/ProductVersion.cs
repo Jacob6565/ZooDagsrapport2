34947fd54d1e4d62e27b782f3f69ac8468727362
Anh Tuan Truong <atruon16@student.aau.dk>
2017-11-30 13:58:52 +0100

    should work

---
+++ AalborgZooProjekt/AalborgZooProjekt/Model/ProductVersion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AalborgZooProjekt.Model
+{
+    public class ProductVersion
+    {
+        private bool IsActive { get; set; }
+    }
+
+    
+}
