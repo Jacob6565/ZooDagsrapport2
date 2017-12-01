0353b5c30a645aa863e2e488b28aae341c296d73
Casper <World@DESKTOP-TTS38P3>
2017-12-01 10:28:52 +0100

    added 2 dummy

---
+++ AalborgZooProjekt/AalborgZooProjekt/Model/DummyShopper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AalborgZooProjekt.Model
+{
+    class DummyShopper : DummyEmployee
+    {
+
+        public DummyShopper() : base 
+        {
+
+        }
+
+        private string Username { get; set; }
+        private string Password { get; set; }
+    }
+}
