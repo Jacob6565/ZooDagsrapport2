12b46fdf8d1483e63deb7b30575f599374c10056
Casper <World@DESKTOP-TTS38P3>
2017-12-01 12:53:56 +0100

    added a test

---
+++ AalborgZooProjekt/NUnit.Test/Exampleish/testingtests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using AalborgZooProjekt.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Test
+{
+    [TestFixture]
+    public class testingtests
+    {
+       
+        [Test]
+        public void TestSetProductToActive()
+        {
+            // Arrange
+            DummyProductVersion DPV = new DummyProductVersion(false);
+            
+            // Act
+            DPV.ActivateProduct();
+
+            // Assert
+            Assert.AreEqual(DPV.IsActive, true);
+        }
+
+    }
+}
