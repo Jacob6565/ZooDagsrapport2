2ebf8443f4163755808e3196397e908e6b011a0e
Anh Tuan Truong <atruon16@student.aau.dk>
2017-12-01 10:28:14 +0100

    Test fra 30 november gone late gone viral

--- AalborgZooProjekt/AalborgZooProjekt/ViewModel/MainViewModel.cs
+++ AalborgZooProjekt/AalborgZooProjekt/ViewModel/MainViewModel.cs
@@ -18,25 +18,6 @@ namespace AalborgZooProjekt.ViewModel
         /// </summary>
         public const string WelcomeTitlePropertyName = "WelcomeTitle";
 
-        private string _welcomeTitle = string.Empty;
-
-        /// <summary>
-        /// Gets the WelcomeTitle property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        
-        public string WelcomeTitle
-        {           
-            get
-            {
-                return _welcomeTitle;
-            }
-            set
-            {
-                Set(ref _welcomeTitle, value);
-            }
-        }
-
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -51,10 +32,15 @@ namespace AalborgZooProjekt.ViewModel
                         // Report error here
                         return;
                     }
-
-                    WelcomeTitle = item.Title;
                 });
         }
+
+        // --------------------------------------
+        // Dyrepasser
+        // --------------------------------------
+
+
+
 
         ////public override void Cleanup()
         ////{
