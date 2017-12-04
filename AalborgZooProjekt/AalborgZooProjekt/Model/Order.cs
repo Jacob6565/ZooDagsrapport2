2ba62907be28a0314d21e45d3260802460fb702b
Anh Tuan Truong <atruon16@student.aau.dk>
2017-12-04 10:48:50 +0100

    Added tests to OrderTester in NUnit.test. Added methods to Order in Model

--- AalborgZooProjekt/AalborgZooProjekt/Model/Order.cs
+++ AalborgZooProjekt/AalborgZooProjekt/Model/Order.cs
@@ -10,16 +10,27 @@ namespace AalborgZooProjekt.Model
     {
         public string DateOrdered { get; set; }
         public string DateCancelled { get; set; }
-        public string Note { get; set; }
         public string DateCreated { get; set; }
+        public string Note { get; set; }
+        public string OrderedByID { get; set; }
         public string Status { get; set; }
+
+        public Order(string dateCreated)
+        {
+            this.DateOrdered = "Yet to be Ordered";
+            this.DateCancelled = "Not Cancelled";
+            this.DateCreated = dateCreated.ToString();
+            this.OrderedByID = "Rikke";
+            this.Note = "No attached note";
+            this.Status = "At the office";
+        }
 
         public Order(string note, string dateCreated)
         {
-            this.DateOrdered = "Not Ordered";
+            this.DateOrdered = "Yet to be ordered";
             this.DateCancelled = "Not Cancelled";
-            this.Note = note;
             this.DateCreated = dateCreated.ToString();
+            this.Note = note;
             this.Status = "At the office";
         }
 
@@ -27,8 +38,8 @@ namespace AalborgZooProjekt.Model
         {
             this.DateOrdered = "Not Ordered";
             this.DateCancelled = dateCancelled;
-            this.Note = note;
             this.DateCreated = dateCreated.ToString();
+            this.Note = note;
             this.Status = status;
         }
 
@@ -36,14 +47,24 @@ namespace AalborgZooProjekt.Model
         {
             this.DateOrdered = dateOrdered;
             this.DateCancelled = dateCancelled;
-            this.Note = note;
             this.DateCreated = dateCreated.ToString();
+            this.Note = note;
             this.Status = status;
         }
 
         public void CancelOrder()
         {
             this.DateCancelled = DateTime.Today.ToString();
+        }
+
+        public string NoteToOrder()
+        {
+            return this.Note = "Send ASAP";
+        }
+
+        public void RemoveOrderedBySetDepartmentInstead()
+        {
+            this.OrderedByID = "Sydamerika";
         }
     }
 }
--- AalborgZooProjekt/NUnit.Test/OrderTester.cs
+++ AalborgZooProjekt/NUnit.Test/OrderTester.cs
@@ -15,14 +15,54 @@ namespace NUnit.Test
         [TestCase()]
         public void CancelOrder()
         {
-            var ordered = DateTime.Today.AddDays(-1).ToString();
-            var created = DateTime.Today.AddDays(-4).ToString();
+            var dateCreated = DateTime.Today.ToString();
 
-            Order order = new Order("Careful of the bananas", created);
+            Order testOrder = new Order(dateCreated);
 
-            order.CancelOrder();
-            Assert.AreEqual(DateTime.Today.ToString(), order.DateCancelled);
+            testOrder.CancelOrder();
+            Assert.AreEqual(DateTime.Today.ToString(), testOrder.DateCancelled);
         }
 
+        [TestCase()]
+        public void AddNoteToOrder()
+        {
+            var dateCreated = DateTime.Today.ToString();
+
+
+            Order testOrder = new Order(dateCreated);
+            testOrder.NoteToOrder();
+
+            Assert.IsNotEmpty(testOrder.Note);
+        }
+
+        //[TestCase()]
+        //public void AutoSaveOrder()
+        //{
+        //    DateTime date = new DateTime(2, 12, 2017);
+        //    var dateCreated = date.ToString();
+
+        //    Order testOrder = new Order(dateCreated);
+
+        //    Assert.IsTrue();
+        //}
+
+        [TestCase()]
+        public void RemoveEmployeeFromOrder()
+        {
+            var dateCreated = DateTime.Today.ToString();
+
+            Order testOrder = new Order(dateCreated);
+
+            testOrder.RemoveOrderedBySetDepartmentInstead();
+            Assert.AreEqual("Sydamerika", testOrder.OrderedByID);
+        }
+
+        [TestCase()]
+        public void AddEmployeeToOrder()
+        {
+            var dateCreated = DateTime.Today.ToString();
+
+            Order testOrder = new Order(dateCreated);
+        }
     }
 }
