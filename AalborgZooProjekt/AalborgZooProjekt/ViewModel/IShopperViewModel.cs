e113e9c354828e0df50b21830bf481e9e7aa08d3
Casper <ccch16@student.aau.dk>
2017-12-05 14:46:52 +0100

    Interface for both Shopper and Zookeeper is filled out with void Name();

--- AalborgZooProjekt/AalborgZooProjekt/ViewModel/IShopperViewModel.cs
+++ AalborgZooProjekt/AalborgZooProjekt/ViewModel/IShopperViewModel.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AalborgZooProjekt.Model;
 
 namespace AalborgZooProjekt.ViewModel
 {
     interface IShopperViewModel
     {
+        void ActivateProduct();
+        void DeActivateProduct();
+        void RemoveUnitFromProduct(DummyProductVersion product);
+        void RemoveOrderLine();
+        void SaveShoppingList();
+        void GetDepartmentSpecificProducts();
+        void GetShoppingListFromHistory();
+        void GetShoppingListHistory();
+        void GetCurrentShoppingList();
+        void GetProducts();
+        void MakeCompleteShoppingList();
+        void CloseProgram();
+        void EstablishDepartment();
+        void EstablishZookeeper();
+        void EstablishShopper();
+        void EstablishShoppingList();
+        void EstablishProduct();
+        void DeleteDepartment();
+        void DeleteOrder();
+        void DeleteZookeeper();
+        void DeleteShopper();
+        void DeleteProduct();
+        void SearchForProduct();
+        void AddOrderLine();
+        void AddProductUnit();
+        void CheckShoppingList();
+        void CheckIfOrderMayVary();
+        void CheckIfOrderExist();
+        void ExecuteShoppingList();
+        void ChangeNumberForOrderLine();
+        void ChangeUnitForOrderLine();
+        void ChangeProductSupplier();
+        void ChangeProductName();
     }
 }
--- AalborgZooProjekt/AalborgZooProjekt/ViewModel/IZookeeperViewModel.cs
+++ AalborgZooProjekt/AalborgZooProjekt/ViewModel/IZookeeperViewModel.cs
@@ -8,6 +8,28 @@ namespace AalborgZooProjekt.ViewModel
 {
     interface IZookeeperViewModel
     {
-
+        void CancelOrder();
+        void RemoveOrderLine();
+        void RemoveZookeeperFromOrder();
+        void RemoveDepartmentSpecificProduct();
+        void SaveComment();
+        void SaveOrder();
+        void GetOrderFromHistory();
+        void GetOrderHistory();
+        void GetZookeepers();
+        void GetProducts();
+        void GetCurrentOrder();
+        void CloseProgram();
+        void MakeOrder();
+        void SearchForProduct();
+        void AddDepartmentSpecificProduct();
+        void AddOrderLine();
+        void AttachZookeeperToOrder();
+        void CheckIfOrderIsAllowedToBeSent();
+        void CheckIfOrderIsAllowedToBeFilledOut();
+        void CheckIfProductIsActive();
+        void ChangeNumberForOrderLines();
+        void ChangeDepartmentForZookeeper();
+        void ChangeUnitForOrderLine();
     }
 }
