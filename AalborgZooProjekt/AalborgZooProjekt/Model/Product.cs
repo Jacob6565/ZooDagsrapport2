01ca679d64ea281515f9e827b08e30e4665cb566
Anh Tuan Truong <atruon16@student.aau.dk>
2017-12-07 12:59:06 +0100

    Set to partial

--- AalborgZooProjekt/AalborgZooProjekt/Model/Product.cs
+++ AalborgZooProjekt/AalborgZooProjekt/Model/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AalborgZooProjekt.Model
 {
-    class Product
+    partial class Product
     {
     }
 }
--- AalborgZooProjekt/AalborgZooProjekt/Model/Zookeeper.cs
+++ AalborgZooProjekt/AalborgZooProjekt/Model/Zookeeper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AalborgZooProjekt.Model
 {
-    class Zookeeper
+    partial class Zookeeper
     {
     }
 }
