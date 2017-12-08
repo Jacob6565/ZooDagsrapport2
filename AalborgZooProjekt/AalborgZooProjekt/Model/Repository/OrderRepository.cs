9ced1248b80d6c55c1e454090c9a078d25f8fc4c
KVISDAOWNER <krijen16@student.aau.dk>
2017-12-08 10:50:19 +0100

    Tried to make database functionality in class OrderRepository

---
+++ AalborgZooProjekt/AalborgZooProjekt/Model/Repository/OrderRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AalborgZooProjekt.Model
+{
+    class OrderRepository : IOrderRepository
+    {
+        public void AddOrder(Order order)
+        {
+            using (var db = new AalborgZooContainer1())
+            {
+                db.OrderSet.Add(order);
+                db.SaveChanges();
+            }
+        }
+
+        public Order GetOrder(int orderID)
+        {
+            using (var db = new AalborgZooContainer1())
+            {
+                var result = db.OrderSet.SingleOrDefault(b => b.Id == orderID);
+
+                return result;
+            }
+        }
+
+
+        public void UpdateOrder(Order order)
+        {
+            using (var db = new AalborgZooContainer1())
+            {
+                var result = db.OrderSet.SingleOrDefault(b => b.Id == order.Id);
+                if (result != null)
+                {
+                    result = order;
+                    db.SaveChanges();
+                }
+            }
+        }
+
+    }
+}
