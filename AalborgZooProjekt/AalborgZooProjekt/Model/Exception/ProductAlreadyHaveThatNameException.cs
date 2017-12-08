a7cb164b223440d841166b21a2a4063af783b7e3
JG <jsande16@student.aau.dk>
2017-12-08 12:53:18 +0100

    Before merge

---
+++ AalborgZooProjekt/AalborgZooProjekt/Model/Exception/ProductAlreadyHaveThatNameException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AalborgZooProjekt.Model
+{
+    [Serializable]
+    internal class ProductAlreadyHaveThatNameException : Exception
+    {
+        public ProductAlreadyHaveThatNameException()
+        {
+        }
+
+        public ProductAlreadyHaveThatNameException(string message) : base(message)
+        {
+        }
+
+        public ProductAlreadyHaveThatNameException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ProductAlreadyHaveThatNameException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
